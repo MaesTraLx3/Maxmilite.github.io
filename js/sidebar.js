998a408eb0b3f7f615459b2bacfdc20f1380396a
Maxmilite <yufe008@live.com>
2021-01-20 23:14:14 +0800

    2021年1月20日23:14:13

---
+++ js/sidebar.js
@@ -0,0 +1,16 @@
+function nav(liID,ulID) 
+{
+    var oLi = document.getElementById(liID);
+    var oUl = document.getElementById(ulID);
+    oLi.onmouseover = function () 
+    {
+        oUl.style.height = '130px';
+    };
+    oLi.onmouseout = function () 
+    {
+        oUl.style.height = window.innerWidth;
+    }
+}
+nav('li1','ul1');
+nav('li2','ul2');
+nav('li3','ul3');
